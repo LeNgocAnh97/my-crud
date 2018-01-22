7b34e8f25a0d930ea5c6f8970295065f422742e0
DocLong <tongduc315@gmail.com>
2018-01-23 03:02:59 +0700

    first commit

---
+++ src/SayHello.php
@@ -0,0 +1,10 @@
+<?php
+namespace tongvanduc\SayHello;
+
+class SayHello
+{
+    public static function world()
+    {
+        return 'Xin chào các bạn! Rất vui khi các bạn sử dụng package của Tôi!!!';
+    }
+}
